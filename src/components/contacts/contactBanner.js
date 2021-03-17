8abeecb2a5dd85a4eeb079f94e69d93c3f31f9ee
binothird <albinoiii.cordova@gmail.com>
2021-03-18 04:05:15 +0800

    updated orders

--- src/components/contacts/contactBanner.js
+++ src/components/contacts/contactBanner.js
@@ -28,7 +28,7 @@ const ContactBanner = () => {
         <div className="mx-auto md:p-16 h-full flex flex-col justify-center font-secondary text-orange-450 text-center">
           <div className="p-4 w-max-w bg-white">
             <h4 className="m-4 md:m-8 text-3xl sm:text-4xl tracking-wide">
-              Our Team is happy to answer your questions
+              Happy to answer your questions
             </h4>
             <h4 className="m-4 md:my-8 text-sm sm:text-base font-primary">
               We'll be in touch as soon as possible
