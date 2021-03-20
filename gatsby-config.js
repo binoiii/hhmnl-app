998c12c0895fe0eb1cc1420d32d080f0a6e03895
binothird <albinoiii.cordova@gmail.com>
2021-03-20 23:08:01 +0800

    updated logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `HappyHandsMnl`,
+    description: `HappyHandsMnl provides a wide variety of sustainable products, which are all heart and happy hands crafted. We offer high quality products yet affordable for all. Engraved. Personalized. Eco-friendly.  Mini Brush. Comb for Gents. Comb with Handle. Wooden Mirror. Stainless Straw Set. Sandalwood Fan. Paddle Brush. Bamboo Tumbler. Metal Vaccum Tumbler. Bamboo Mug. Bambaunan Bento Lunch Box. Bambootensils. Bamboo Toothbrush. Bamboo Notebook. Bamboo Sunglasses. Bamboo Chopping Board. Wooden Items. Wooden Pen. Wooden Bottle Opener
+    Wooden Phone Stand Keychain. Wooden Speaker and Phone Holder. Leather Goods. Leather Chord Organizer. Cord Organizer. Leather Keyholder. Leather Passport Holder. Coin Purse. Handy Wallet. Long Wallet. Flask Set. Glass Cup w/ Bamboo Lid. Gift. Souvenir. Present. Afforadable.`,
+    author: `@binoiii`,
   },
   flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
@@ -18,22 +19,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `HappyHandsMnl`,
+        short_name: `hhmnl`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ca7919`,
+        theme_color: `#ca7919`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`,
+        icon: `src/images/hhmnl-icon.png`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-preload-fonts`,
-
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
