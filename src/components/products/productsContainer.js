27c5f0283b7049dce4171655f5b1b69f58e154e7
binothird <albinoiii.cordova@gmail.com>
2021-03-24 01:28:52 +0800

    added pagination

--- src/components/products/productsContainer.js
+++ src/components/products/productsContainer.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { connect } from "react-redux"
+import PropTypes from "prop-types"
 
 import ProductCard from "./productCard"
 import { fetchProducts } from "../../redux/products/productsActions"
@@ -10,13 +11,16 @@ const ProductsContainer = ({
 }) => {
   useEffect(() => fetchProducts(), [fetchProducts])
 
+  const [visibleProducts, setVisibleProducts] = useState(6)
+  const handleSeeMore = () => setVisibleProducts(visibleProducts + 3)
+
   return (
     <div
       id="products"
-      className="mt-32 mb-12 sm:mb-8 md:mb-0 flex flex-col justify-center items-center"
+      className="mt-32 mb-16 flex flex-col justify-center items-center"
     >
       <h2 className="mb-8 font-secondary text-orange-450 text-4xl">Products</h2>
-      <div className="flex flex-wrap justify-center">
+      <div>
         {loading ? (
           <h2 className="mx-4 my-16 text-center font-primary text-gray-700 text-2xl font-bold tracking-wide">
             Loading...
@@ -28,13 +32,28 @@ const ProductsContainer = ({
           </h2>
         ) : (
           products &&
-          products.length > 0 &&
-          products.map(productDetails => (
-            <ProductCard
-              key={productDetails.productID}
-              productDetails={productDetails}
-            />
-          ))
+          products.length > 0 && (
+            <div className="flex flex-col">
+              <div className="flex flex-wrap justify-center">
+                {products
+                  .map(productDetails => (
+                    <ProductCard
+                      key={productDetails.productID}
+                      productDetails={productDetails}
+                    />
+                  ))
+                  .splice(0, visibleProducts)}
+              </div>
+              {visibleProducts < products.length && (
+                <button
+                  className="mx-auto py-4 w-40 bg-white border border-orange-450 font-primary text-xs text-orange-450 uppercase tracking-wider outline-none focus:outline-none"
+                  onClick={handleSeeMore}
+                >
+                  More products
+                </button>
+              )}
+            </div>
+          )
         )}
       </div>
     </div>
@@ -48,5 +67,12 @@ const mapStateToProps = ({ products }) => ({
 const mapDispatchToProps = dispatch => ({
   fetchProducts: () => dispatch(fetchProducts()),
 })
+
+ProductsContainer.propTypes = {
+  loading: PropTypes.bool.isRequired,
+  products: PropTypes.array,
+  error: PropTypes.bool.isRequired,
+  fetchProducts: PropTypes.func,
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProductsContainer)
