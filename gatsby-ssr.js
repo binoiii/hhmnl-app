c4fca085e31966cf4166caec0dbc735473e9e015
binothird <albinoiii.cordova@gmail.com>
2021-03-24 20:46:16 +0800

    added store and fb messenger

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,11 @@
+import React from "react"
+import FBMessenger from "./src/components/fbMessenger"
 import reduxWrapper from "./src/redux/reduxWrapper"
 export const wrapRootElement = reduxWrapper
+
+export const wrapPageElement = ({ element }) => (
+  <React.Fragment>
+    {element}
+    <FBMessenger />
+  </React.Fragment>
+)
