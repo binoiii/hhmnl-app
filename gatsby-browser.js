183d7107452ee05ce4f72481a28524e79ea90d2c
binothird <albinoiii.cordova@gmail.com>
2021-03-24 20:39:23 +0800

    add fb

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,3 +2,10 @@ import React from "react"
 import FBMessenger from "./src/components/fbMessenger"
 import reduxWrapper from "./src/redux/reduxWrapper"
 export const wrapRootElement = reduxWrapper
+
+export const wrapPageElement = ({ element }) => (
+  <React.Fragment>
+    {element}
+    <FBMessenger />
+  </React.Fragment>
+)
