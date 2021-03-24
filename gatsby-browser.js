a92508fe1a5f6cd02850a3672396762c5f6813c8
binothird <albinoiii.cordova@gmail.com>
2021-03-24 19:52:53 +0800

    added fb messenger and max width

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,13 @@
 import reduxWrapper from "./src/redux/reduxWrapper"
+
+const React = require("react")
+const FBMessenger = require("./src/components/fbMessenger").default
+
 export const wrapRootElement = reduxWrapper
+
+exports.wrapPageElement = ({ element }) => (
+  <React.Fragment>
+    {element}
+    <FBMessenger />
+  </React.Fragment>
+)
---
+++ src/components/fbMessenger.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react"
+
+const FBMessenger = () => {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v9.0",
+      })
+    }
+    ;(function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) return
+      js = d.createElement(s)
+      js.id = id
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"
+      fjs.parentNode.insertBefore(js, fjs)
+    })(document, "script", "facebook-jssdk")
+  })
+  return (
+    <>
+      <div id="fb-root" />
+      <div
+        className="fb-customerchat"
+        attribution="install_email"
+        page_id="303146620547418"
+        // page_id="2469960713229077"
+        logged_in_greeting="Happy day to you! Drop your questions and we will get back to you the soonest."
+        logged_out_greeting="Thank you for visiting HappyHandsMnl!"
+        theme_color="#ca7919"
+      />
+    </>
+  )
+}
+
+export default FBMessenger
