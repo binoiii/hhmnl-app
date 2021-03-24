dd9117ba4007ace7b5249527c1fdf612463c45b7
binothird <albinoiii.cordova@gmail.com>
2021-03-24 20:08:50 +0800

    updated import function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import React from "react"
 import reduxWrapper from "./src/redux/reduxWrapper"
 import FBMessenger from "./src/components/fbMessenger"
 
-exports.wrapRootElement = reduxWrapper
+export const wrapRootElement = reduxWrapper
 
 exports.wrapPageElement = ({ element }) => (
   <React.Fragment>
